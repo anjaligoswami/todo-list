336ad2cb27aead8a810921dcdd193bbf2e13fbfe
anjaligoswami <agcodes86400@gmail.com>
2021-12-09 16:31:23 +0530

    ui improved and added the saving feature

--- script.js
+++ script.js
@@ -2,47 +2,65 @@ let addToDoButton = document.getElementById('addTask');
 let todoContainer = document.getElementById('todoContainer');
 let inputField = document.getElementById('inputField');
 
-let todoList = JSON.parse(localStorage.getItem ("todoList")) || [];
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
 
-function delete_todo(event){
-           paragraph = event.target
-for(let i=0; i<todoList.length; i++)
-{
-    let todo = todoList[i];
-
-    var paragraph = document.createElement("li");
-    
-    paragraph.innerText = todo;
-    paragraph.classList.add('paragraph-styling')
-    todoContainer.appendChild(paragraph)
-    paragraph.addEventListener("click", function () {
-        paragraph.style.textDecoration = "line-through"
-    })
-
-    paragraph.addEventListener("dblclick", function () {
-        todoContainer.removeChild(paragraph)
-    })
-
+function deleteTodo(event) {
+  const paragraph = event.target
+  paragraph.parentNode.removeChild(paragraph)
+  const currentTodo = paragraph.innerText
+  console.log(currentTodo)
+  todoList = todoList.filter(todo => {
+    return todo.todo !== currentTodo
+  })
+  localStorage.setItem("todoList", JSON.stringify(todoList));
 }
-addToDoButton.addEventListener("click", function () {
-    if (inputField.value == "")
-        return;
-    let todo = inputField.value;
+
+function addTodo(todo) {
+  const paragraph = document.createElement("p")
+  paragraph.innerText = todo.todo
+  paragraph.classList.add('paragraph-styling')
+  todoContainer.appendChild(paragraph)
+  inputField.value = ""
+  if(todo.done){
+    paragraph.classList.toggle("todo-done")
+  }
+  paragraph.addEventListener("click", function (event) {
+    todoList = todoList.map(todo => {
+      if(todo.todo === event.target.innerText){
+        return {
+          ...todo,
+          done: !todo.done
+        }
+      }
+      return todo
+    })
     console.log(todoList)
-    todoList.push(todo);
     localStorage.setItem("todoList", JSON.stringify(todoList));
+    paragraph.classList.toggle("todo-done")
+  })
+  paragraph.addEventListener("dblclick", deleteTodo)
+}
 
-    var paragraph = document.createElement("li");
-    
-    paragraph.innerText = inputField.value;
-    paragraph.classList.add('paragraph-styling')
-    todoContainer.appendChild(paragraph)
-    inputField.value = ""
-    paragraph.addEventListener("click", function () {
-        paragraph.style.textDecoration = "line-through"
-    })
+addToDoButton.addEventListener("click", function () {
+  // return if todo is empty
+  if (!inputField.value)
+    return;
+  let todo = inputField.value;
 
-    paragraph.addEventListener("dblclick", function () {
-        todoContainer.removeChild(paragraph)
-    })
+  // don't add if already added
+  for (let i = 0; i < todoList.length; i++) {
+    let todo1 = todoList[i];
+    if(todo1 === todo)
+      return
+  }
+
+  // save todo list
+  todoList.push({todo, done: false});
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+
+  addTodo({todo, done: false})
 })
+
+for (let i = 0; i < todoList.length; i++) {
+  addTodo(todoList[i])
+}
